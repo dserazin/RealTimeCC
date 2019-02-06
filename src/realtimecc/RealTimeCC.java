387f7d69ee7f3be6f2ee3a8a6e2ac444cc0b3be0
Danse <1310ns14@gmail.com>
2019-02-06 10:37:08 +0100

    Komment

--- src/realtimecc/RealTimeCC.java
+++ src/realtimecc/RealTimeCC.java
@@ -8,28 +8,11 @@ package realtimecc;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import static realtimecc.Admin.Admin;
-import static realtimecc.Datenbank.aendereDatensatz;
-import static realtimecc.Datenbank.erstelleDatensatz;
-import static realtimecc.Datenbank.erstelleTabelle;
-import static realtimecc.Datenbank.erstelleVerbindung;
-import static realtimecc.Datenbank.loescheDatensatz;
-import static realtimecc.Datenbank.zeigeDatensatz;
 import static realtimecc.Login.Login;
-import static realtimecc.Logout.Logout;
 
 /**
  *
