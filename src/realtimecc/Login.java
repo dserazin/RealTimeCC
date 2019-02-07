37adf9eeb7272d2ec45b5245c125a9e9bce8e7ef
Danse <1310ns14@gmail.com>
2019-02-07 12:25:27 +0100

    Komment

--- src/realtimecc/Login.java
+++ src/realtimecc/Login.java
@@ -10,10 +10,13 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import static realtimecc.Datenbank.zeigeDatensatz;
 import static realtimecc.Logout.Logout;
 
 /**
@@ -56,11 +59,12 @@ public class Login
         // DateTime________________
             Date date1 = new Date();
        JLabel jl0 = new JLabel();
-            jl0.setText("" +date1.toString());
+            jl0.setText(""+date1.toString());
             jl0.setBounds(100, 300, 400, 90);
             jl0.setForeground(Color.red);
             jl0.setFont(new Font("areal",0,20));
             jf.add(jl0);
+            
             
             // Zweite Reihe : Eingabe Fachrichtung / Textfeld                   
        JLabel jl2 = new JLabel();
@@ -76,7 +80,7 @@ public class Login
             jf.add(jtf2);
             
             // Schaltfläche Berechnung
-       JButton jcb1 = new JButton("SignIn", null);
+       JButton jcb1 = new JButton("LogIn", null);
             jcb1.setForeground(Color.blue);
             jcb1.setBackground(Color.red);
             jcb1.setBounds(245, 120, 100, 30);
@@ -84,14 +88,40 @@ public class Login
             
             
            jcb1.addActionListener((ActionEvent e) -> 
-           {
+  {
                Logout();  
                Date date2 = new Date();
-              jl0.setText("Login Time "+date2.toString());
-           });
+              jl0.setText("Logi Time "+date2.toString());
+              
+//            try {
+//                zeigeDatensatz(jtf1,jtf2);
+//                System.out.println("Hallo Herr ");
+//            } catch (Exception ex) {
+//                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+            
+//          //__Login Daten mit Datenbank abgleichen_______
+//                 if (true) 
+//                 {
+//                     
+//                 } 
+//                 else 
+//                 {
+//                     
+//                 }     
+          
+           
+ });
+          
+        
+           
 //_______________________________________________________________________            
             jf.repaint();
        jf.setVisible(true);
+    }
+
+    private static void zeigeDatensatz(JTextField jtf1, JTextField jtf2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
